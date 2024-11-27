d351a643fe819f08623b49ff6cfa5174ca6d175a
vipboy2903200321phu <vipboy2903200321phu@gmail.com>
2024-11-27 10:32:17 +0000

    build ui for chat overview

--- lib/pages/body_message.dart
+++ lib/pages/body_message.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:phan_mem_giao_nhac_viec/components/my_message_overview_tile.dart';
 
 class BodyMessage extends StatelessWidget {
   const BodyMessage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("body msg"),
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: ListView.builder(
+          itemCount: 4,
+          itemBuilder: (context, index) {
+            return const MyMessageOverviewTile(
+              chatName: "Test Chat Name",
+              msg: "test msg 😍",
+            );
+          },
+        ),
     );
   }
 }
