edfc6c9d6e14a0e11e584e7262bc9853c79dcca7
vipboy2903200321phu <vipboy2903200321phu@gmail.com>
2024-11-27 14:21:19 +0000

    create message overview and add chat floating button

--- lib/pages/body_message.dart
+++ lib/pages/body_message.dart
@@ -8,15 +8,68 @@ class BodyMessage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8),
-      child: ListView.builder(
-          itemCount: 4,
-          itemBuilder: (context, index) {
-            return const MyMessageOverviewTile(
-              chatName: "Test Chat Name",
-              msg: "test msg 😍",
-            );
-          },
-        ),
+      child: Stack(
+        children: [
+          // list message
+          ListView.builder(
+            itemCount: 3,
+            itemBuilder: (context, index) {
+              return const MyMessageOverviewTile(
+                chatName: "Test Chat Name",
+                msg: "test msg 😍",
+              );
+            },
+          ),
+          // float add new chat button
+          Positioned(
+            bottom: 10,
+            right: 10,
+            child: FloatingActionButton(
+              onPressed: () {
+                AddNewChatDialog(context);
+              },
+              child: const Icon(Icons.add),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<dynamic> AddNewChatDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Add new Chat!"),
+          content: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Enter chat name",
+                ),
+              ),
+              // search user to add to this chat
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Search user to add",
+                ),
+              ),
+            ],
+          ),
+          // save or cancel action
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text("Add"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
