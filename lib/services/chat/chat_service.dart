20f9bda802df8a34138eaf3acd5ebe3becab141e
vipboy2903200321phu <vipboy2903200321phu@gmail.com>
2024-11-28 03:33:41 +0000

    add streambuilder to display overview chat group

--- lib/services/chat/chat_service.dart
+++ lib/services/chat/chat_service.dart
@@ -1,7 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatService {
-  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  static final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  static final _collectioName = "Chat";
+
+  static Stream<QuerySnapshot> groupChatStream() {
+    return _firebaseFirestore
+        .collection(_collectioName)
+        .orderBy('timeUpdate', descending: true)
+        .snapshots();
+  }
 
   Future<void> createNewChat() async {
     
