63a4b6a29a0abba8c58f30eff071bee8cc9ab1fa
ha <hnngcs2@gmail.com>
2024-12-04 16:36:48 +0700

    sdfgg

--- lib/pages/workspace_page.dart
+++ lib/pages/workspace_page.dart
@@ -1,6 +1,7 @@
 import 'package:calendar_view/calendar_view.dart';
 import 'package:flutter/material.dart';
 import 'package:phan_mem_giao_nhac_viec/components/my_loremipsum.dart';
+import 'package:provider/provider.dart';
 
 class WorkspacePage extends StatefulWidget {
   const WorkspacePage({super.key});
@@ -10,6 +11,28 @@ class WorkspacePage extends StatefulWidget {
 }
 
 class _WorkspacePageState extends State<WorkspacePage> {
+  final _now = DateTime.now();
+  List<CalendarEventData> _events = [
+    CalendarEventData(
+      date: DateTime.now(),
+      title: "Project meeting",
+      description: "Today is project meeting.",
+      startTime: DateTime(DateTime.now().year, DateTime.now().month,
+          DateTime.now().day, 18, 30),
+      endTime: DateTime(
+          DateTime.now().year, DateTime.now().month, DateTime.now().day, 22),
+    ),
+  ];
+  final event = CalendarEventData(
+    date: DateTime.now(),
+    title: "Project meeting",
+    description: "Today is project meeting.",
+    startTime: DateTime(
+        DateTime.now().year, DateTime.now().month, DateTime.now().day, 18, 30),
+    endTime: DateTime(
+        DateTime.now().year, DateTime.now().month, DateTime.now().day, 22),
+  );
+
   @override
   Widget build(BuildContext context) {
     return CalendarControllerProvider(
@@ -24,6 +47,12 @@ class _WorkspacePageState extends State<WorkspacePage> {
               SizedBox(
                 height: 700,
                 child: MonthView(),
+              ),
+              TextButton(
+                onPressed: () {
+                  CalendarControllerProvider.of(context).controller.add(event);
+                },
+                child: Text("asdfsd"),
               ),
               Text(MyLoremipsum.loremIpsum),
             ],
