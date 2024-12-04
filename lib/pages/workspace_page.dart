8bb21a868f27444eb70638f9e5b45609983d7fa5
ha <hnngcs2@gmail.com>
2024-12-04 16:56:07 +0700

    add CalendarControllerProvider in main app

--- lib/pages/workspace_page.dart
+++ lib/pages/workspace_page.dart
@@ -12,17 +12,7 @@ class WorkspacePage extends StatefulWidget {
 
 class _WorkspacePageState extends State<WorkspacePage> {
   final _now = DateTime.now();
-  List<CalendarEventData> _events = [
-    CalendarEventData(
-      date: DateTime.now(),
-      title: "Project meeting",
-      description: "Today is project meeting.",
-      startTime: DateTime(DateTime.now().year, DateTime.now().month,
-          DateTime.now().day, 18, 30),
-      endTime: DateTime(
-          DateTime.now().year, DateTime.now().month, DateTime.now().day, 22),
-    ),
-  ];
+
   final event = CalendarEventData(
     date: DateTime.now(),
     title: "Project meeting",
@@ -35,28 +25,25 @@ class _WorkspacePageState extends State<WorkspacePage> {
 
   @override
   Widget build(BuildContext context) {
-    return CalendarControllerProvider(
-      controller: EventController(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Workspace name"),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 700,
-                child: MonthView(),
-              ),
-              TextButton(
-                onPressed: () {
-                  CalendarControllerProvider.of(context).controller.add(event);
-                },
-                child: Text("asdfsd"),
-              ),
-              Text(MyLoremipsum.loremIpsum),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Workspace name"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 600,
+              child: MonthView(),
+            ),
+            TextButton(
+              onPressed: () {
+                // CalendarControllerProvider.of(context).controller.addAll(_events);
+              },
+              child: Text("asdfsd"),
+            ),
+            Text(MyLoremipsum.loremIpsum),
+          ],
         ),
       ),
     );
